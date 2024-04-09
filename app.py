8892caaa90b2364aa280d4ed5470df98f26a47ec
makiwarner <144996342+makiwarner@users.noreply.github.com>
2024-04-09 19:26:38 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import openai
+
+st.title("Purrfect Matchmaker, at your service.")
+
+# Setup the Open AI API Python Client
+client = openai.OpenAI(api_key="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")
+
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What kind of pet are you looking for?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
